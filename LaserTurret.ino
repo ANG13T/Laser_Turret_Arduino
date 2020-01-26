ecd836de08bbfcc04b51f7439c6443ad404034fa
angelina-tsuboi <angelina.t1832@gmail.com>
2020-01-26 08:16:17 -0800

    Initial Commit

---
+++ LaserTurret.ino
@@ -0,0 +1,15 @@
+#include <Servo.h>
+
+int angle;
+Servo bottomServo;
+Servo topServo; 
+
+void setup() {
+  bottomServo.attach(13);
+  topServo.attach(12);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}

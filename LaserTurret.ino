8e82f82207a5dbeb14806f2f1014bf61e44ef969
angelina-tsuboi <angelina.t1832@gmail.com>
2020-01-26 08:26:40 -0800

    Servo Functionality

--- LaserTurret.ino
+++ LaserTurret.ino
@@ -2,7 +2,13 @@
 
 int angle;
 Servo bottomServo;
-Servo topServo; 
+Servo topServo;
+const int joyY = 10;
+const int joyX = 11;
+int joyValY;
+int joyValX;
+int bottomMoveVal;
+int topMoveVal;
 
 void setup() {
   bottomServo.attach(13);
@@ -11,5 +17,10 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  joyValY = analogRead(joyY);
+  joyValX = analogRead(joyX);
+  bottomMoveVal = map(joyValX, 0, 1023, 0, 180);
+  topMoveVal = map(0, joyValY, 1023, 0, 180);
+  bottomServo.write(bottomMoveVal);
+  topServo.write(topMoveVal);
 }
